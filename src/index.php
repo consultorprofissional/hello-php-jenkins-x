d0037dcc78b1ab1239a510cf9138eec3bb1e8672
flavio.mendes <flavio.mendes@viverebrasil.com.br>
2020-06-10 12:14:01 -0300

    Adicionado o monolog

--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
+
 echo "<b>Hello!!!</b>, Jenkins-x <br />";
 ?>
